e2d315b0df0dc99bda69440ea222e8fcddfb7f81
moiup <moiuppol@gmail.com>
2025-09-25 21:47:13 +0300

    Homework Lesson 13 exceptions task 1

---
+++ src/Homework13/WrongPasswordException.java
@@ -0,0 +1,11 @@
+package Homework13;
+
+public class WrongPasswordException extends Exception{
+    public WrongPasswordException() {
+        super("Неверный пароль");
+    }
+
+    public WrongPasswordException(String message) {
+        super(message);
+    }
+}
