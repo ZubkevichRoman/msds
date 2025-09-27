caf27dbc2d1d7ae95de559ce9d30c57137419684
moiup <moiuppol@gmail.com>
2025-09-27 11:46:16 +0300

    Homework Lesson 14 file task 1

---
+++ src/Homework14/Main.java
@@ -0,0 +1,32 @@
+package Homework14;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        /**
+         * В задании приложен файл Ромео и Джульетта на английском, вычитать его в Idea.
+         * Проанализировать и записать в другой файл самое длинное слово.
+         */
+        File input = new File("D:\\romeo-and-juliet");
+        String mostLongestWord = "";
+        try (Scanner scanner = new Scanner(input)) {
+            while (scanner.hasNext()) {
+                String word = scanner.next();
+                if (word.length() > mostLongestWord.length()) {
+                    mostLongestWord = word;
+                }
+            }
+        }catch (IOException e){
+            throw new IOException(e.getMessage());
+        }
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("D:\\output"))){
+            bufferedWriter.write(mostLongestWord);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("Most longest word: " + mostLongestWord);
+    }
+}
